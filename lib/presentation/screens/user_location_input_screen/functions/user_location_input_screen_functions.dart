4e30a5241264eb5f737f218136ef015d13271aaf
MD. Sajid Hossain <sajidrec@gmail.com>
2024-10-05 23:19:03 +0600

    code refactor

--- lib/presentation/screens/user_location_input_screen/functions/user_location_input_screen_functions.dart
+++ lib/presentation/screens/user_location_input_screen/functions/user_location_input_screen_functions.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_location_marker/presentation/screens/map_screen/map_screen.dart';
-import 'package:flutter_osm_location_marker/presentation/state_holders/map_screen_controller.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:latlong2/latlong.dart';
 
 class UserLocationInputScreenFunctions {
+  static Future<LatLng?> _convertAddressToLatLong(String address) async {
+    try {
+      List<Location> locations = await locationFromAddress(address);
+      if (locations.isNotEmpty) {
+        return LatLng(locations.first.latitude, locations.first.longitude);
+      }
+    } catch (e) {
+      return null;
+    }
+    return null;
+  }
+
   static void onSearchButtonClick({
     required GlobalKey<FormState> formKey,
     required BuildContext context,
@@ -12,7 +24,7 @@ class UserLocationInputScreenFunctions {
     FocusScope.of(context).unfocus();
     if (formKey.currentState!.validate()) {
       LatLng? latLongByUserInput =
-          await MapScreenController.convertAddressToLatLong(address ?? "");
+          await _convertAddressToLatLong(address ?? "");
 
       if (latLongByUserInput != null) {
         if (context.mounted) {
